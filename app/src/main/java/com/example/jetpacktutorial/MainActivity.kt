1d527a69368624cc2735fd9a5d07a9e20d4a9564
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-11-01 03:22:47 +0530

    State

--- app/src/main/java/com/example/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktutorial/MainActivity.kt
@@ -3,99 +3,158 @@ package com.example.jetpacktutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.jetpacktutorial.ui.theme.JetpackTutorialTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var painter = painterResource(id = R.drawable.photo)
-            var contentDescription = "Google form that was filled for a company "
-            var title = "Filled Form"
-            ImageCard(painter = painter, contentDescription = contentDescription, title = title)
-        }
-    }
-}
-
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = Modifier.fillMaxWidth(0.5f).padding(10.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
-    ) {
-        Box(
-            modifier = Modifier.height(200.dp)
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent,
-                        Color.Black
-                    ),
-                )))
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-               verticalArrangement = Arrangement.Bottom
-            ) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-                Text(text = contentDescription, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                Modifier.fillMaxSize()
+            ){
+                val color = remember{
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    modifier = Modifier.fillMaxSize().weight(1f)
+                ){
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier.fillMaxSize().background(color.value).weight(1f)
+                )
             }
+
         }
     }
-
 }
-
-@Preview(device = "id:pixel_5")
 @Composable
-fun ImageCardPreview() {
-    var painter = painterResource(id = R.drawable.photo)
-    var contentDescription = "filled form "
-    var title = "Title"
-    ImageCard(painter = painter, contentDescription = contentDescription, title = title)
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color)->Unit){
+
+
+        Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                Color.Black
+            )
+            .clickable {
+                updateColor(
+                     Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+
+            }
+    )
 }
+//    @Preview
+//    @Composable
+//    private fun someFunWithText() {
+//        Box(
+//            modifier = Modifier.fillMaxSize(),
+//            contentAlignment = Alignment.Center
+//        ){
+//            Text(
+//                text = buildAnnotatedString {
+//                    withStyle(
+//                        style = SpanStyle(
+//                            color = Color.Red
+//                        )
+//                    ){
+//                        append("This is ")
+//                    }
+//                    withStyle(
+//                        style = SpanStyle(color = Color.White)
+//                    ){
+//                        append("my")
+//                    }
+//                    withStyle(
+//                        style = SpanStyle(color = Color.Green)
+//                    ){
+//                        append("india")
+//                    }
+//
+//
+//                }
+//            )
+//        }
+//
+//
+//
+//    }
+
+
+//@Composable
+//fun ImageCard(
+//    painter: Painter,
+//    contentDescription: String,
+//    title: String,
+//    modifier: Modifier = Modifier
+//) {
+//    Card(
+//        modifier = Modifier.fillMaxWidth(0.5f).padding(10.dp),
+//        shape = RoundedCornerShape(15.dp),
+//        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+//    ) {
+//        Box(
+//            modifier = Modifier.height(200.dp)
+//        ) {
+//            Image(
+//                painter = painter,
+//                contentDescription = contentDescription,
+//                contentScale = ContentScale.Crop
+//            )
+//            Box(modifier = Modifier
+//                .fillMaxSize()
+//                .background(Brush.verticalGradient(
+//                    colors = listOf(
+//                        Color.Transparent,
+//                        Color.Black
+//                    ),
+//                )))
+//            Column(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .padding(12.dp),
+//               verticalArrangement = Arrangement.Bottom
+//            ) {
+//                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+//                Text(text = contentDescription, style = TextStyle(color = Color.White, fontSize = 16.sp))
+//            }
+//        }
+//    }
+//
+//}
+//
+//@Preview(device = "id:pixel_5")
+//@Composable
+//fun ImageCardPreview() {
+//    var painter = painterResource(id = R.drawable.photo)
+//    var contentDescription = "filled form "
+//    var title = "Title"
+//    ImageCard(painter = painter, contentDescription = contentDescription, title = title)
+//}
